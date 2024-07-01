85561b3f26464c6fa89c47041ae187ad85851402
YeYu <haosax@qq.com>
2024-07-01 21:20:45 +0800

    up

--- main.go
+++ main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
-	"wxcloudrun-golang/db"
-	"wxcloudrun-golang/service"
 )
 
 func main() {
-	if err := db.Init(); err != nil {
-		panic(fmt.Sprintf("mysql init failed with %+v", err))
-	}
 
-	http.HandleFunc("/", service.IndexHandler)
-	http.HandleFunc("/api/count", service.CounterHandler)
-
-	log.Fatal(http.ListenAndServe(":80", nil))
 	r := gin.Default()
 	r.GET("/good", func(c *gin.Context) {
 		c.JSON(200, gin.H{})
